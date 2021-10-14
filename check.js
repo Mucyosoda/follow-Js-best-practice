d397ef0deb1e83dfb3f7f7ab3573742734da0450
Mucyosoda <mucyosofa@gmail.com>
2021-10-14 11:31:02 +0100

    follow js best practice

---
+++ check.js
@@ -0,0 +1,14 @@
+const taskCompleted = (e, items) => {
+  const complete = e.target;
+  items = [...JSON.parse(localStorage.getItem('items'))];
+
+  complete.parentElement.classList.add('completed');
+  for (let i = 0; i < items.length; i += 1) {
+    if (complete.parentNode.id === items[i].id.toString()) {
+      items[i].completed = complete.checked;
+    }
+  }
+  localStorage.setItem('items', JSON.stringify(items));
+};
+
+export default taskCompleted;
